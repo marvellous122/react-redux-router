ad65506b93dfcca08cd750425f1026e238ea90b1
Passion <marvellouspeak@gmail.com>
2017-11-10 13:06:36 -0800

    integrate scss and navigation

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Route, Link} from 'react-router-dom';
+import { Route, NavLink} from 'react-router-dom';
 
-import './style.css';
+import './style.scss';
 
 import Home from '../home';
 import About from '../about';
@@ -10,18 +10,20 @@ const App = () => (
   <div className="App">
     <header>
       <div className="container">
-        <a className="logo" href="#">giant<strong>SEA</strong></a>
-        <nav className="navbar pull-right">
-          <Link to="/">Home</Link>
-          <Link to="/about-us">About</Link>
+        <div className="navbar nav-brand">
+          <a className="logo" href="/">up<strong>Pe@k</strong></a>
+        </div>
+        <nav className="pull-right">
+          <NavLink to="/" activeClassName="active">HOME</NavLink>
+          <NavLink to="/about-us" activeClassName="active">ABOUT</NavLink>
         </nav>
       </div>
     </header>
 
-    <main>
+    <div className="container">
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
-    </main>
+    </div>
   </div>
 )
 
